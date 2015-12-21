1a5a3fb6865394bebc6f42ed37dc19e8fe305695
soyoda <lcrespin@soyoda.com>
2015-12-21 18:05:39 -0500

    css referencias,coordenadas

--- public/jquery/APP.js
+++ public/jquery/APP.js
@@ -289,15 +289,3 @@ function fecActual() {
 }
 
 
- function getLocation()
-        {
-            if (navigator.geolocation)
-                navigator.geolocation.getCurrentPosition(showPosition);
-            else
-                alert(("Geolocation is not supported by this browser"));
-        }
-        function showPosition(position)
-        {
-            alert("Latitude: " + position.coords.latitude);
-            alert("Longitude: " + position.coords.longitude); 
-        }
