c16e3be940f074d1d4585c6ef25a7ea1aa4acce0
soyoda <lcrespin@soyoda.com>
2015-12-21 18:45:01 -0500

    getlocation

--- public/jquery/gps.js
+++ public/jquery/gps.js
@@ -1,15 +1,19 @@
 
 
-	 function getLocation()
-        {
-            if (navigator.geolocation)
+ function getLocation(){
+       if (navigator.geolocation){
                 navigator.geolocation.getCurrentPosition(showPosition);
-            else
-                alert(("Geolocation is not supported by this browser"));
-        }
-        
+       }else{
+                alert("Geolocation is not supported by this browser");
+       }
+    }
+
         function showPosition(position)
         {
             alert("Latitude: " + position.coords.latitude);
             alert("Longitude: " + position.coords.longitude); 
+           return  1
         }
+
+
+           
