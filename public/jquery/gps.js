abea84d96941519c8cc321aef12677274217b79d
soyoda <lcrespin@soyoda.com>
2015-12-22 08:36:58 -0500

    testgps

--- public/jquery/gps.js
+++ public/jquery/gps.js
@@ -1,19 +1,26 @@
 
-
+           
  function getLocation(){
        if (navigator.geolocation){
                 navigator.geolocation.getCurrentPosition(showPosition);
-       }else{
-                alert("Geolocation is not supported by this browser");
        }
+       return null;
     }
 
         function showPosition(position)
         {
             alert("Latitude: " + position.coords.latitude);
-            alert("Longitude: " + position.coords.longitude); 
-           return  1;
+            alert("Longitude: " + position.coords.longitude);           
         }
 
 
-           
+    function getVarsLocation(){
+       var vars={};
+       if (navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(function(position){
+            vars["latitude"]=position.coords.latitude;
+            vars["longitude"]=position.coords.longitude;  
+          });
+       }
+       return vars;
+    }
