615f97c2e3f51715df4a3f31cd6a8e8ba418858c
soyoda <lcrespin@soyoda.com>
2015-12-22 09:02:35 -0500

    base de datos 1

--- public/jquery/Sync.js
+++ public/jquery/Sync.js
@@ -2,7 +2,7 @@
 var syncServer = 'http://186.5.36.149:94/SAC/Sync';
 var PROJECT_ID_GOOGLE = "994360885610";
 var admPass = "sa";
-var DATABASE_NAME="SAC2";
+var DATABASE_NAME="SAC";
 var DATABASE_VERSION="1.0";
 var DATABASE_DESCRIPTION="SAC Gestion Ventas Soyoda";
 var DATABASE_SIZE=200000;
