dbc24c8358d8a1d945c4ab5f9f6bc31d08beabe9
soyoda <lcrespin@soyoda.com>
2015-12-22 10:23:03 -0500

    gps final test

--- public/jquery/gps.js
+++ public/jquery/gps.js
@@ -14,27 +14,27 @@ var vars_position={};
             alert("Longitude: " + position.coords.longitude);           
         }
 
-var onSuccess = function(position) {
-    vars_position={'latitude: '          : position.coords.latitude,
+function onSuccess(position) {
+    callback({'latitude: '          : position.coords.latitude,
           'longitude'         : position.coords.longitude         ,
           'altitude'          : position.coords.altitude          ,
           'accuracy'          : position.coords.accuracy         ,
           'altitude Accuracy' : position.coords.altitudeAccuracy  ,
           'heading'           : position.coords.heading          ,
           'speed'             : position.coords.speed             ,
-          'timestamp'         : new Date(position.timestamp)     };
-};
+          'timestamp'         : new Date(position.timestamp)     });        
+}
 
 function onError(error) {
     alert('code: '    + error.code    + '\n' +
           'message: ' + error.message + '\n');
 }
 
-
+function callback(new_vars_position) {
+   vars_position=new_vars_position;
+}
 
     function getVarsLocation(){
-       navigator.geolocation.getCurrentPosition(onSuccess, onError);
+       navigator.geolocation.getCurrentPosition(onSuccess,onError);
        return vars_position;
     }
-
-     
