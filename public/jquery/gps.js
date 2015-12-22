bf34c872aa2dbda68673667e5b08563720180f9c
soyoda <lcrespin@soyoda.com>
2015-12-22 13:26:22 -0500

    gmaps prueba

--- public/jquery/gps.js
+++ public/jquery/gps.js
@@ -1,5 +1,47 @@
+  
 
-var vars_position={};
+  function GPS(){
+    this.positions={};
+    this.state="none";
+    this.message="ok";
+    }
+
+  GPS.prototype.updateLocationPositions = function() {
+    GMaps.geolocate({
+            success: function(position) {
+              if (position.coords!=null){
+                this.positions["latitude"]=position.coords.latitude;
+                this.positions["longitude"]=position.coords.longitude;
+                this.positions["accuracy"]=position.coords.accuracy;
+                this.positions["altitude"]=position.coords.altitude;
+                this.positions["altitudeAccuracy"]=position.coords.altitudeAccuracy;
+                this.positions["heading"]=position.coords.heading;
+              }
+            
+            },
+            error: function(error) {
+               this.state="error";
+               this.message=error.message;
+            },
+            not_supported: function() {
+               this.state="not_supported";
+               this.message="Your browser does not support geolocation";
+               this.positions={};
+            },
+            always: function() {
+               this.state="done";
+               this.message="ok";
+      }
+    });
+  }
+
+
+  GPS.prototype.getLocationPositions = function() {
+    return this.positions;
+  }
+
+ 
+/*var vars_position={};
            
  function getLocation(){
        if (navigator.geolocation){
@@ -14,27 +56,31 @@ var vars_position={};
             alert("Longitude: " + position.coords.longitude);           
         }
 
-function onSuccess(position) {
-    callback({'latitude'          : position.coords.latitude,
+function onSuccessGEO(position) {
+    vars_position={'latitude'          : position.coords.latitude,
           'longitude'         : position.coords.longitude         ,
           'altitude'          : position.coords.altitude          ,
           'accuracy'          : position.coords.accuracy         ,
           'altitude Accuracy' : position.coords.altitudeAccuracy  ,
           'heading'           : position.coords.heading          ,
           'speed'             : position.coords.speed             ,
-          'timestamp'         : new Date(position.timestamp)     });        
+          'timestamp'         : new Date(position.timestamp)     };
+             alert("Latitude: " + position.coords.latitude);
+            alert("Longitude: " + position.coords.longitude);    
 }
 
-function onError(error) {
+// onError Callback receives a PositionError object
+//
+function onErrorGEO(error) {
     alert('code: '    + error.code    + '\n' +
           'message: ' + error.message + '\n');
 }
 
-function callback(new_vars_position) {
-   vars_position=new_vars_position;
-}
+
 
     function getVarsLocation(){
-       navigator.geolocation.getCurrentPosition(onSuccess,onError);
-       return vars_position;
-    }
+      navigator.geolocation.getCurrentPosition(function(position) {
+  onSuccessGEO( position.coords.latitude);
+});
+      return 1; 
+    }*/
