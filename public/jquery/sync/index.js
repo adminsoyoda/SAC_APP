466ccb4d9f723899676e58fb7dab52989de7b06b
soyoda <lcrespin@soyoda.com>
2016-01-08 09:15:13 -0500

    e.message

--- public/jquery/sync/index.js
+++ public/jquery/sync/index.js
@@ -56,6 +56,7 @@ var app = {
                 }
                 break;
             case 'message':
+                alert(e.message);
                 message_sync_div.html(e.message);                    
                 break;
             case 'error':
