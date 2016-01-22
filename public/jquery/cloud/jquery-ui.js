d57facd12bc32f0e3c302aa8487b171767b55aeb
adminsoyoda <lcrespin@soyoda.com>
2016-01-22 09:05:15 -0500

    Update jquery-ui.js

--- public/jquery/cloud/jquery-ui.js
+++ public/jquery/cloud/jquery-ui.js
@@ -7553,8 +7553,8 @@ $.extend( $.ui.autocomplete, {
 	filter: function( array, term ) {
 		/*var matcher = new RegExp( $.ui.autocomplete.escapeRegex( term ), "i" );
 		return $.grep( array, function( value ) {
-			return matcher.test( value.label || value.value || value );*/
-		});
+			return matcher.test( value.label || value.value || value );
+		});*/
 	}
 });
 
